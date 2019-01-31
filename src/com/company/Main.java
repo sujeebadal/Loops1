ac63ea458782c2ff0eb994f8ffb0902e68f1f8e5
sujeebadal <sujeebadal@gmail.com>
2019-01-31 15:38:39 -0500

    added new changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,37 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         for (int i=1; i<=10; i++){
             System.out.println(i);
         }
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        //odd numbers
+
+        for (int i=1; i<=20; i++){
+            if (i%2!=0)
+                System.out.println(i);
+        }
+
+        //even numbers
+
+        for (int i=1; i<=20; i++){
+            if (i%2==0)
+                System.out.println(i);
+        }
+
+
     }
 }
+
+
 
 
 
